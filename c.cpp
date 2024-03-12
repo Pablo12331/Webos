478cd690f325e1bb029b4a789074b73a5a7341cf
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2024-03-12 23:45:20 +0000

    genere cambios jijijija

---
+++ c.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int edad;
+    string sexo;
+    float altura;
+
+    cout<<"Deme su edad: "; cin>> edad;
+    cout<<"Digame su sexo: "; cin>> sexo;
+    cout<<"Deme su estatura: "; cin>> altura;
+
+    cout<<"\nSu edad es: "<< edad 
+        << "\nSu sexo es: "<< sexo 
+        <<"\nSu estatura es: "<< altura <<endl;
+
+    return 0;
+}
